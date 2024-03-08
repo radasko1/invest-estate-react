a033737c58f8fed27885e77d079b5db1db82626e
Radim Kolar <radimkolar22@gmail.com>
2024-03-08 11:26:36 +0100

    refactor usePropertyFilter hook

--- src/hooks/usePropertyFilter.hook.ts
+++ src/hooks/usePropertyFilter.hook.ts
@@ -6,16 +6,25 @@ export const usePropertyFilter = (
   filter: FilterModel,
   propertyList: EstatePropertyModel[],
 ): EstatePropertyModel[] => {
+  // execute when 'filter' is changed
   return React.useMemo(() => {
     return propertyList.filter((property) => {
-      const { minRooms, maxRooms, type, minPrice, maxPrice } = filter;
-      return (
-        (minRooms < 0 || property.roomCount >= minRooms) &&
-        (maxRooms < 0 || property.roomCount <= maxRooms) &&
-        (!type || property.type === type) &&
-        (minPrice < 0 || property.monthRental >= minPrice) &&
-        (maxPrice < 0 || property.monthRental <= maxPrice)
-      );
+      // default values (init values, after reset)
+      const priceFilterDefaultValue = filter.minPrice < 0;
+      const roomNumberFilterDefaultValue = filter.minRooms < 0;
+      const typeDefaultValue = !filter.type;
+
+      const isRoomCountMatch =
+        roomNumberFilterDefaultValue ||
+        (property.roomCount >= filter.minRooms && filter.maxRooms < 0) ||
+        property.roomCount <= filter.maxRooms;
+      const isTypeMatch = typeDefaultValue || property.type === filter.type;
+      const isPriceMatch =
+        priceFilterDefaultValue ||
+        (property.monthRental >= filter.minPrice && filter.maxPrice < 0) ||
+        property.monthRental <= filter.maxPrice;
+
+      return isRoomCountMatch && isTypeMatch && isPriceMatch;
     });
   }, [filter, propertyList]);
 };
